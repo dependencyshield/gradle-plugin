f57a9faef8ee09f9b6137324fede379bbc53e38d
Lukas Krecan <lukas@krecan.net>
2023-03-29 13:58:09 +0200

    Do not rewrite original setting

--- src/main/java/com/dependencyshield/gradle/ConfigureDependencyCheck.java
+++ src/main/java/com/dependencyshield/gradle/ConfigureDependencyCheck.java
@@ -8,7 +8,14 @@ import org.jetbrains.annotations.NotNull;
 import org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static org.owasp.dependencycheck.reporting.ReportGenerator.Format.HTML;
+import static org.owasp.dependencycheck.reporting.ReportGenerator.Format.JSON;
 
 public abstract class ConfigureDependencyCheck extends DefaultTask {
     private final Property<String> organizationId;
@@ -29,10 +36,16 @@ public abstract class ConfigureDependencyCheck extends DefaultTask {
     @TaskAction
     public void configureDependencyCheck()  {
         Action<DependencyCheckExtension> action = ext -> {
-            ext.setSuppressionFiles(List.of(getSuppressionUrl()));
-            ext.setFormats(List.of("HTML", "JSON"));
+            ext.setSuppressionFiles(append(ext.getSuppressionFiles(), List.of(getSuppressionUrl())));
+            ext.setFormats(append(ext.getFormats(), List.of(HTML.toString(), JSON.toString())));
         };
         getProject().getExtensions().configure("dependencyCheck", action);
+    }
+
+    private <T> List<T> append(Collection<T> original, Collection<T> newElements) {
+        Set<T> set = new HashSet<>(original);
+        set.addAll(newElements);
+        return new ArrayList<>(set);
     }
 
     @NotNull
